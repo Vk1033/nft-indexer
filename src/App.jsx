b7f7054e40c21308de51df5cdb4bf02a02a20028
Vk1033 <kinkarvansh@gmail.com>
2024-08-15 01:04:25 +0530

    AU-week-6

--- src/App.jsx
+++ src/App.jsx
@@ -1,115 +1,101 @@
-import {
-  Box,
-  Button,
-  Center,
-  Flex,
-  Heading,
-  Image,
-  Input,
-  SimpleGrid,
-  Text,
-} from '@chakra-ui/react';
-import { Alchemy, Network } from 'alchemy-sdk';
-import { useState } from 'react';
+import { Box, Button, Center, Flex, Heading, Image, Input, SimpleGrid, Text, CircularProgress } from "@chakra-ui/react";
+import { Alchemy, Network } from "alchemy-sdk";
+import { useState } from "react";
 
 function App() {
-  const [userAddress, setUserAddress] = useState('');
+  const [userAddress, setUserAddress] = useState("");
   const [results, setResults] = useState([]);
   const [hasQueried, setHasQueried] = useState(false);
   const [tokenDataObjects, setTokenDataObjects] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [ifError, setIfError] = useState(false);
 
-  async function getNFTsForOwner() {
+  async function getWalletNFTs() {
+    setHasQueried(false);
+    setLoading(true);
+    if (!window.ethereum.selectedAddress) {
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+    }
+    getNFTsForOwner(null, window.ethereum.selectedAddress);
+  }
+
+  async function getNFTsForOwner(_, queryAddress = userAddress) {
+    setHasQueried(false);
+    setLoading(true);
     const config = {
-      apiKey: '<-- COPY-PASTE YOUR ALCHEMY API KEY HERE -->',
+      apiKey: import.meta.env.VITE_ALCHEMY_API_KEY,
       network: Network.ETH_MAINNET,
     };
 
     const alchemy = new Alchemy(config);
-    const data = await alchemy.nft.getNftsForOwner(userAddress);
+    let data;
+    try {
+      data = await alchemy.nft.getNftsForOwner(queryAddress);
+    } catch (e) {
+      console.error(e);
+      setIfError(true);
+      setLoading(false);
+      return;
+    }
     setResults(data);
 
     const tokenDataPromises = [];
 
-    for (let i = 0; i < data.ownedNfts.length; i++) {
-      const tokenData = alchemy.nft.getNftMetadata(
-        data.ownedNfts[i].contract.address,
-        data.ownedNfts[i].tokenId
-      );
+    for (let i = 0; i < Math.min(data.ownedNfts.length, 10); i++) {
+      const tokenData = alchemy.nft.getNftMetadata(data.ownedNfts[i].contract.address, data.ownedNfts[i].tokenId, {});
       tokenDataPromises.push(tokenData);
     }
 
     setTokenDataObjects(await Promise.all(tokenDataPromises));
     setHasQueried(true);
+    setLoading(false);
+    setIfError(false);
   }
   return (
     <Box w="100vw">
       <Center>
-        <Flex
-          alignItems={'center'}
-          justifyContent="center"
-          flexDirection={'column'}
-        >
+        <Flex alignItems={"center"} justifyContent="center" flexDirection={"column"}>
           <Heading mb={0} fontSize={36}>
             NFT Indexer 🖼
           </Heading>
-          <Text>
-            Plug in an address and this website will return all of its NFTs!
-          </Text>
+          <Text>Plug in an address and this website will return all of its NFTs!</Text>
         </Flex>
       </Center>
-      <Flex
-        w="100%"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent={'center'}
-      >
-        <Heading mt={42}>Get all the ERC-721 tokens of this address:</Heading>
-        <Input
-          onChange={(e) => setUserAddress(e.target.value)}
-          color="black"
-          w="600px"
-          textAlign="center"
-          p={4}
-          bgColor="white"
-          fontSize={24}
-        />
+      <Flex w="100%" flexDirection="column" alignItems="center" justifyContent={"center"}>
+        <Heading mt={42}>Get the NFTs (ERC-721 tokens) of this address (10 due to ratelimit) :</Heading>
+        <Input onChange={(e) => setUserAddress(e.target.value)} color="black" w="600px" textAlign="center" p={4} bgColor="white" fontSize={24} />
+        {ifError ? <Text color="red">Error fetching NFTs</Text> : null}
         <Button fontSize={20} onClick={getNFTsForOwner} mt={36} bgColor="blue">
           Fetch NFTs
         </Button>
-
-        <Heading my={36}>Here are your NFTs:</Heading>
+        <Button fontSize={20} onClick={getWalletNFTs} mt={36} bgColor="blue">
+          Fetch Wallet NFTs
+        </Button>
 
         {hasQueried ? (
-          <SimpleGrid w={'90vw'} columns={4} spacing={24}>
+          <SimpleGrid w={"90vw"} columns={4} spacing={24}>
+            {results.ownedNfts.length === 0 ? (
+              <Heading my={36} justifySelf="center">
+                No NFTs found
+              </Heading>
+            ) : (
+              <Heading my={36}>Here are your NFTs:</Heading>
+            )}
             {results.ownedNfts.map((e, i) => {
+              if (tokenDataObjects[i] === undefined) return;
               return (
-                <Flex
-                  flexDir={'column'}
-                  color="white"
-                  bg="blue"
-                  w={'20vw'}
-                  key={e.id}
-                >
+                <Flex flexDir={"column"} color="white" bg="blue" w={"20vw"} key={i}>
                   <Box>
-                    <b>Name:</b>{' '}
-                    {tokenDataObjects[i].title?.length === 0
-                      ? 'No Name'
-                      : tokenDataObjects[i].title}
+                    <b>Name:</b> {tokenDataObjects[i].title?.length === 0 ? "No Name" : tokenDataObjects[i].title}
                   </Box>
-                  <Image
-                    src={
-                      tokenDataObjects[i]?.rawMetadata?.image ??
-                      'https://via.placeholder.com/200'
-                    }
-                    alt={'Image'}
-                  />
+                  <Image src={tokenDataObjects[i]?.rawMetadata?.image ?? "https://via.placeholder.com/200"} alt={"Image"} />
                 </Flex>
               );
             })}
           </SimpleGrid>
-        ) : (
-          'Please make a query! The query may take a few seconds...'
-        )}
+        ) : loading ? (
+          <CircularProgress margin={100} isIndeterminate />
+        ) : null}
       </Flex>
     </Box>
   );
